9b95fdcabe9bce6f10b2226f122ae820cda0ef2c
Trian <triannurizkillah@gmail.com>
2026-05-17 22:59:07 +0700

    feat: update event back to message

--- gateway-rust/src/feature/realtime/mod.rs
+++ gateway-rust/src/feature/realtime/mod.rs
@@ -16,19 +16,33 @@ pub struct RegisterPublicSse {
 }
 
 use axum::http::HeaderMap;
+use log::{error, info};
 
 pub async fn register_public_sse(
     state: State<AppState>,
     query: Query<RegisterPublicSse>,
-) -> (HeaderMap, Sse<impl Stream<Item = Result<Event, Infallible>>>) {
+) -> (
+    HeaderMap,
+    Sse<impl Stream<Item = Result<Event, Infallible>>>,
+) {
     let mut headers = HeaderMap::new();
     headers.insert("X-Accel-Buffering", "no".parse().unwrap());
     headers.insert("Cache-Control", "no-cache".parse().unwrap());
 
     let sse = state
         .sse
-        .new_client(query.user_id.clone(), query.device_id.clone(), state.model_info.clone())
+        .new_client(
+            query.user_id.clone(),
+            query.device_id.clone(),
+            state.model_info.clone(),
+        )
         .await;
+    if query.user_id.is_empty() || query.device_id.is_empty() {
+        error!("User ID and Device ID cannot be empty rejecting stream");
+        let _ = state.sse
+            .reject_client()
+            .await.expect("Failed reject");
+    }
 
     (headers, sse)
 }
